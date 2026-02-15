2317e797d88ea0247ae5f5be8d74abe47f3c9a6a
losched16 <clint.losch@gmail.com>
2026-02-15 05:38:38 -0500

    Create [id]/page.tsx

---
+++ app/api/swing-analysis/[id]/page.tsx
@@ -0,0 +1,11 @@
+SwingAnalysisView from '@/components/SwingAnalysisView'
+
+export default function SwingAnalysisPage({ 
+  params 
+}: { 
+  params: { id: string } 
+}) {
+  return (
+    <div className="max-w-4xl mx-auto py-8 px-4">
+      <SwingAnalysisView analysisId={params.id} />
+    </div>
